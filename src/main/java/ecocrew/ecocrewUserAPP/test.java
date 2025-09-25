3a00871436431190d5a6f2f20f44e050aec36826
Nikhat <nikhat.mantiqh@gmail.com>
2025-09-25 17:26:55 +0530

    test

--- src/main/java/ecocrew/ecocrewUserAPP/test.java
+++ src/main/java/ecocrew/ecocrewUserAPP/test.java
@@ -4,6 +4,8 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		WebDriverManager.c
 		System.out.println("Hello!:Testing CICD automation");
 
 	}
